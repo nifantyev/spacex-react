cc190fee1cb1d34f00a6c87e40e432b9a73a8329
Eugene Nifantyev <nifantyev@gmail.com>
2020-10-14 21:50:49 +0300

    Convert App to class component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <>      
-      <Header />
-      <Main />
-      <Features />
-    </>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <>      
+        <Header />
+        <Main />
+        <Features />
+        <Footer />
+      </>
+    );  
+  }
 }
 
 export default App;
