549942649b48f041c24575b7b16d7c515e09db36
Евгений Нифантьев <nifantyev@gmail.com>
2020-10-15 11:58:01 +0300

    Add fetching data from SpaceX API

--- src/App.js
+++ src/App.js
@@ -5,19 +5,44 @@ import Main from './components/Main/Main';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
 
+import FetchData from './service/FetchData';
 
 class App extends React.Component {
 
+  fetchData = new FetchData();
+
   state = {
-    rocket: 'Falcon 1'
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: []
+  }
+
+  componentDidMount() {
+    this.updateRocket(); 
+  }
+
+  updateRocket() {
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) });
+        return data;
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({ rocketFeatures }));
+  }
+
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRocket);
   }
 
   render() {
     return (
       <>      
-        <Header />
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket} />
         <Main rocket={this.state.rocket} />
-        <Features />
+        <Features features={this.state.rocketFeatures} />
         <Footer />
       </>
     );  
