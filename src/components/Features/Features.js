afd3b74f8db34ca40196b0a4b8e8c29ee8b332aa
Евгений Нифантьев <nifantyev@gmail.com>
2020-10-15 12:01:10 +0300

    Dynamically change rocket features in Feature component

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -2,10 +2,10 @@ import React from 'react';
 import RellaxWrapper from 'react-rellax-wrapper';
 import './features.css';
 
-const Features = () => (
+const Features = ({features}) => (
 	<section className="features">
 		<h2 className="features-title">
-			Falcon 1 <br/>Overview
+			{features && features.name} <br/>Overview
 		</h2>
 		<div className="overview">
 
@@ -16,34 +16,39 @@ const Features = () => (
 				<thead>
 					<tr>
 						<td className="table-column">HEIGHT</td>
-						<td className="table-column">22.25 m / 73 ft</td>
+						<td className="table-column">{features && features.height.meters} m / {features && features.height.feet} ft</td>
 					</tr>
 					<tr>
 						<td className="table-column">DIAMETER</td>
-						<td className="table-column">1.68 m / 5.5 ft</td>
+						<td className="table-column">{features && features.diameter.meters} m / {features && features.diameter.feet} ft</td>
 					</tr>
 					<tr>
 						<td className="table-column">MASS</td>
-						<td className="table-column">30,146 kg / 66,460 lb</td>
+						<td className="table-column">
+							{features && features.mass.kg.toLocaleString()} kg / {features && features.mass.lb.toLocaleString()} lb
+						</td>
 					</tr>
 					<tr>
 						<td className="table-column">PAYLOAD TO LEO</td>
-						<td className="table-column">450 kg / 992 lb</td>
+						<td className="table-column">
+							{features && features.payload_weights.find(weight => weight.id === 'leo').kg} kg
+							&nbsp;/&nbsp;
+							{features && features.payload_weights.find(weight => weight.id === 'leo').lb} lb
+						</td>
 					</tr>
 				</thead>
 			</table>
             <RellaxWrapper speed={14}>
                 <img
-                        src="./img/falcon-1.png"
-                        alt="rocket"
-                        className="rocket"
-                        data-rellax-speed="14"
+					src={`./img/${features && features.name.toLowerCase().replace(' ', '-')}.png`}
+					alt="rocket"
+					className="rocket"
                 />
             </RellaxWrapper>
 			<article>
 				<h3 className="features-subtitle">DESCRIPTION</h3>
 				<p className="features-text">
-					The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.
+					{features && features.description}					
 				</p>
 			</article>
 		</div>
