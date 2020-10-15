0162fab8631a040c68507877b53962b8e73c8726
Евгений Нифантьев <nifantyev@gmail.com>
2020-10-15 12:00:38 +0300

    Dynamically change video in Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './main.css';
 
+const video = {
+    'Falcon 1': 'moon',
+    'Falcon 9': 'earth',
+    'Falcon Heavy': 'mars',
+    other: 'space'
+  };
+
 const Main = ({rocket}) => (
     <section className="main">
         <h1 className="title">
         { rocket }
         </h1>
         <div className="video-container">
-        <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
+        <video 
+            className="video" 
+            autoPlay loop muted 
+            src={`./video/${video.hasOwnProperty(rocket) ? video[rocket]: video.other}.mp4`}></video>
         </div>
     </section>
 );
